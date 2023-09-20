693d2278e7c68aa0aa4feae448939564077e41d3
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2023-09-20 10:26:46 +0200

    add tailwind script/style

--- src/main/resources/vue/page-welcome.vue
+++ src/main/resources/vue/page-welcome.vue
@@ -1,5 +1,12 @@
 <template id="page-welcome">
-    <h1>Hello World</h1>
+    <div class="grid place-content-center h-full">
+        <a class="text-3xl font-bold rounded-md border border-transparent bg-g px-8 py-3 text-white hover:bg-indigo-700">
+            Hello World
+        </a>
+        <div class="text-3xl text-">
+
+        </div>
+    </div>
 </template>
 <script>
 app.component("page-welcome", {
