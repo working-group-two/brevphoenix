1de53910a142ada9922a4f166f7ab36fded1ada5
Anders Wold Eldhuset <andereld@cisco.com>
2024-09-10 14:14:24 +0200

    Tiny whitespace tweaks

--- src/main/resources/vue/conversation.vue
+++ src/main/resources/vue/conversation.vue
@@ -3,16 +3,16 @@
     <div v-for="(message, index) in messages" class="flex flex-col p-4" :class="{ 'mt-auto': index === 0 }">
       <div v-if="message.direction === 'FROM_SUBSCRIBER'" class="flex flex-row-reverse">
         <div class="flex flex-col">
-          <div class="text-orange-100 text-xs max-w self-end">{{ dateTime(message.timestamp) }}</div>
-          <div class="bg-orange-950 text-orange-100 p-2 rounded max-w-prose whitespace-pre">
+          <div class="text-orange-100 text-xs max-w self-end mb-1">{{ dateTime(message.timestamp) }}</div>
+          <div class="bg-orange-950 text-orange-100 p-3 rounded-lg max-w-prose whitespace-pre leading-4">
             {{ message.content }}
           </div>
         </div>
       </div>
       <div v-else class="flex flex-row">
         <div class="flex flex-col">
-          <div class="text-orange-100 text-xs">{{ dateTime(message.timestamp) }}</div>
-          <div class="bg-gray-800 text-orange-100 p-2 rounded max-w-prose whitespace-pre">
+          <div class="text-orange-100 text-xs mb-1">{{ dateTime(message.timestamp) }}</div>
+          <div class="bg-gray-800 text-orange-100 p-3 rounded-lg max-w-prose whitespace-pre leading-4">
             {{ message.content }}
           </div>
         </div>
