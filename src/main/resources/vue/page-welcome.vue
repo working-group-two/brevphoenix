ef69af6d625178c706aac5a23cfbfd10ba58a1b4
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2024-09-11 11:45:10 +0200

    brian suggestion

--- src/main/resources/vue/page-welcome.vue
+++ src/main/resources/vue/page-welcome.vue
@@ -25,7 +25,7 @@
         <div class="flex flex-col max-w-lg items-center justify-center">
           <img src="/favicon.jpeg" alt="decorative phoenix" width="1024" height="1024"
                class="px-2 w-lg max-w-full rounded-3xl block">
-          <h2 class="text-3xl mt-4">SMS has rerisen</h2>
+          <h2 class="text-3xl mt-4">SMS has risen from the ashes</h2>
           <p class="text-lg text-orange-600">Select a conversation or create a new one</p>
         </div>
       </div>
